1f6ba1c35b27e6a17645594d2f32892b2b3b2edd
mrama030 <mrama030@uottawa.ca>
2019-02-18 17:47:57 -0500

    Lessons 1 and 2.1 complete 100%

---
+++ src/codility/lessons/Main.java
@@ -0,0 +1,16 @@
+package codility.lessons;
+
+import codility.lessons.Lessons.*;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Lesson 1 - Binary Gap");
+        System.out.println("Testing with value N = 1041, expected result = 5");
+        int lesson1 = Lesson_1.BinaryGap(1041);
+        System.out.println(lesson1 + "\n");
+
+
+    }
+}
